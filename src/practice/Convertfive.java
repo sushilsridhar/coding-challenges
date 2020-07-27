70ce8bccfb0ddabba1e64554d140b5dc70039a34
Sushil <sushilsridhar16@gmail.com>
2020-07-27 08:39:46 +0530

    convert to five

---
+++ src/practice/Convertfive.java
@@ -0,0 +1,50 @@
+package practice;
+
+/*
+    o(n), where n is number of digits in the number
+ */
+public class Convertfive {
+
+    static int convertfive(int num) {
+        int result = 0, count = 1, mod = 0;
+        while(num!=0) {
+            mod = num%10;
+            num = num/10;
+
+            if(mod == 0) {
+                mod = 5;
+            }
+            result = result + mod * count;
+            count = count *10;
+        }
+
+        return result;
+    }
+
+    static int convertFiveRecursion(int num) {
+
+        if(num==0) {
+            return num;
+        } else {
+            return recursion(num);
+        }
+    }
+
+    private static int recursion(int num) {
+
+        if(num == 0) return 0;
+
+        int mod = num%10;
+
+        if (mod == 0) mod = 5;
+
+        num = num/10;
+
+        return recursion(num) * 10 + mod;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertfive(1004));
+        System.out.println(convertFiveRecursion(1004));
+    }
+}
