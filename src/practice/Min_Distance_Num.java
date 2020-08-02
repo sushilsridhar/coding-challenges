df6447aac92a81fa64fe429294617e9bfda7591e
Sushil <sushilsridhar16@gmail.com>
2020-08-02 20:36:14 +0530

    minimium distance between two numbers

--- src/practice/Min_Distance_Num.java
+++ src/practice/Min_Distance_Num.java
@@ -1,4 +1,33 @@
 package practice;
 
+/* minimium distance between two numbers */
+
 public class Min_Distance_Num {
+    public static void main(String[] args) {
+
+        int a[] = { 3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3};
+
+        int x = 3, y = 6;
+
+        System.out.println(minDist(a, a.length, x, y));
+    }
+
+    /*
+    *   o(n2), where n is the length of the array
+    */
+    private static int minDist(int[] a, int n, int x, int y) {
+
+        int minDist = -1;
+
+        for(int i=0; i<n; i++) {
+            for(int j = i+1; j<n; j++) {
+
+                if( (a[i] == x && a[j] == y) || (a[j] == x && a[i] == y) && minDist > Math.abs(i-j)){
+                    minDist = Math.abs(i-j);
+                }
+            }
+        }
+
+        return minDist;
+    }
 }
