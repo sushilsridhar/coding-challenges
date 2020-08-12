97c452cb63e0427b20275f8f953f6f2057ed93c6
Sushil <sushilsridhar16@gmail.com>
2020-08-12 10:42:39 +0530

    Max sum path in two arrays

--- src/arrays/Max_Sum_Path.java
+++ src/arrays/Max_Sum_Path.java
@@ -1,7 +1,63 @@
 package arrays;
 
+/*
+    Time complexity, o(m+n), where m is length of arr1 and n is length of arr2
+
+    Elements of both arrays are processed at same time, while loop runs for m+n times
+ */
 public class Max_Sum_Path {
     public static void main(String[] args) {
 
+        int arr1[] = { 2, 3, 7, 10, 12, 15, 30, 34};
+        int arr2[] = { 1, 5, 7, 8, 10, 15, 16, 19};
+
+        // max sum path --> 1,5,7,8,10,12,15,30,34 = 122
+
+        int result = findMaxPathSum(arr1, arr2);
+        System.out.println(result);
+
+    }
+
+    private static int findMaxPathSum(int arr1[], int arr2[])
+    {
+        int i=0,j=0,result=0,sum1=0,sum2=0;
+
+        while(i<arr1.length && j<arr2.length) {
+
+            if(arr1[i] < arr2[j]) {
+                sum1+= arr1[i];
+                i++;
+            }
+            else if(arr1[i] > arr2[j]) {
+                sum2+= arr2[j];
+                j++;
+            }
+            else if(arr1[i] == arr2[j]) {
+
+                result+= Math.max(sum1,sum2);
+                result+= arr1[i];
+
+                i++;
+                j++;
+
+                sum1=0;
+                sum2=0;
+            }
+        }
+
+        while(i<arr1.length) {
+            sum1+= arr1[i];
+            i++;
+        }
+
+        while(j<arr2.length) {
+            sum2+= arr2[j];
+            j++;
+        }
+
+        result+= Math.max(sum1,sum2);
+
+
+        return result;
     }
 }
