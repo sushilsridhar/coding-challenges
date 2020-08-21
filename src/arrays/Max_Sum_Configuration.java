837a7ba9e72f70e8e682ee88cef1412c1e1f252e
Sushil <sushilsridhar16@gmail.com>
2020-08-22 00:07:34 +0530

    Max sum configuration best solution

--- src/arrays/Max_Sum_Configuration.java
+++ src/arrays/Max_Sum_Configuration.java
@@ -6,9 +6,47 @@ public class Max_Sum_Configuration {
 
        int[] a = {8, 3, 1, 2};
 
-       int result = bruteForce(a, a.length);
+       //int result = bruteForce(a, a.length);
 
-       System.out.println(result);
+        int result = bestSolution(a, a.length);
+
+        System.out.println(result);
+    }
+
+    private static int bestSolution(int[] a, int n) {
+
+        int sum = 0;
+
+        int max = Integer.MIN_VALUE, maxIndex = 0;
+
+        for(int i=0; i<n; i++) {
+            if(a[i] > max) {
+                max=a[i];
+                maxIndex = i;
+            }
+        }
+
+        int numberOfShifts = maxIndex+1; /* since shifts are based on array index, add +1 */
+
+        reverseArray(a, 0, n-1);
+        reverseArray(a, 0, n - 1 - numberOfShifts);
+        reverseArray(a, n - numberOfShifts, n-1);
+
+
+        for(int j=0; j<n; j++) {
+            sum = sum + a[j] * j;
+        }
+
+        return sum;
+    }
+
+    private static void reverseArray(int[] a, int start_index, int end_index) {
+
+        for(int i=start_index; i<end_index; i++, end_index--) {
+            int temp = a[i];
+            a[i] = a[end_index];
+            a[end_index] = temp;
+        }
     }
 
     /*  o(n2) , where n is the size of the array */
