062d0b7aca08825284f7045f981e25f7d967344d
Sushil <sushilsridhar16@gmail.com>
2020-09-01 10:26:05 +0530

    alternative logic for return duplicates

--- src/arrays/Return_Duplicates.java
+++ src/arrays/Return_Duplicates.java
@@ -7,13 +7,44 @@ public class Return_Duplicates {
 
     public static void main(String[] args) {
 
-        int a[] = {26 ,13, 9, 25, 1, 1, 0, 22, 13, 22, 20, 3, 8, 11, 25, 10, 3, 15, 11, 19, 20, 2, 4, 25, 14, 23, 14};
+        //int a[] = {26 ,13, 9, 25, 1, 1, 0, 22, 13, 22, 20, 3, 8, 11, 25, 10, 3, 15, 11, 19, 20, 2, 4, 25, 14, 23, 14};
+
 
         ArrayList<Integer> result = solution(a, a.length);
+
+
+        //int a[] = { 2, 3, 1, 2, 3, 7, 7};
+        //ArrayList<Integer> result = duplicates(a, a.length);
 
         for(int r: result) {
             System.out.print(r+" ");
         }
+    }
+
+    /* not working properly - check */
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+        // First check all the values that are
+        // present in an array then go to that
+        // values as indexes and increment by
+        // the size of array
+        for (int i = 0; i < n; i++) {
+            int index = arr[i] % n;
+            arr[index] += n;
+        }
+
+        // Now check which value exists more
+        // than once by dividing with the size
+        // of array
+        int flag = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] / n) > 1) {
+                ans.add(i);
+                flag = 1;
+            }
+        }
+        if (flag == 0) ans.add(-1);
+        return ans;
     }
 
 
