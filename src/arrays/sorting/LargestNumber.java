2bbdf4057f545fd7161a20415fac531869d572b5
Sushil <sushilsridhar16@gmail.com>
2022-01-13 07:59:59 +0530

    sort by color

---
+++ src/arrays/sorting/LargestNumber.java
@@ -0,0 +1,69 @@
+package arrays.sorting;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+/*
+    Given a array A of non negative integers, arrange them such that they form the largest number.
+
+    Note: The result may be very large, so you need to return a string instead of an integer.
+
+    A = [3, 30, 34, 5, 9]
+    "9534330" - output
+
+    "3" + "30" --> "330"
+    "30" + "3" --> "303"
+
+    330 > 303, which means, 3 should come first in the array and 30 should come after 3 in the array
+    a is 3, b is 30, return -1, so that sort function places a in the front of the array
+
+    edge case, if [0,0,0,0] return [0], skip all leading zeros
+ */
+public class LargestNumber {
+
+    public static void main(String[] args) {
+
+        int[] A = { 93, 2, 37, 639, 8, 100, 345, 49 };
+
+        System.out.println(solution(A));
+    }
+
+    private static String solution(int[] A) {
+
+        String r = "";
+        String[] strArray = new String[A.length];
+        for(int i=0; i<A.length; i++) {
+            strArray[i] = Integer.toString(A[i]);
+        }
+
+        Arrays.sort(strArray, (a, b) -> {
+
+            if(Objects.equals(a,b)) {
+                return 0;
+            } else {
+                String ab = a + b;
+                String ba = b + a;
+
+                if(Long.parseLong(ab) > Long.parseLong(ba)) {
+                    return -1;
+                }
+            }
+            return 1;
+        });
+
+        boolean firstNonZeroElementFound = false;
+        for(int i=0; i<strArray.length-1; i++) {    // length-1 --> edge case
+
+            if(Objects.equals(strArray[i],"0") && !firstNonZeroElementFound) {
+                continue;
+            } else {
+                firstNonZeroElementFound = true;
+            }
+            r = r + strArray[i];
+        }
+
+        r = r + strArray[strArray.length-1]; // edge case
+
+        return r;
+    }
+}
--- src/arrays/sorting/SortByColor.java
+++ src/arrays/sorting/SortByColor.java
@@ -1,37 +1,84 @@
 package arrays.sorting;
 
+/*
+    Given an array with n objects colored red, white or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+    Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+
+    Note: Using library sort function is not allowed.
+
+    A = [0 1 2 0 1 2 1 0 2 1]
+        [0 0 0 1 1 1 1 2 2 2] - output
+
+    optimized:
+    iterate and insert zero at the beginning, and insert two at the end, one will be automatically arranged
+ */
 public class SortByColor {
-    public class Solution {
-        public int[] sortColors(int[] A) {
+    public static void main(String[] args) {
 
-            int n = A.length;
+        int[] A = {0, 1, 2, 0, 1, 2, 1, 0, 2, 1};
 
-            if(n == 1) {
-                return A;
-            }
-
-            if(n == 2) {
-                A[0] = 0;
-                A[1] = 1;
-                return A;
-            }
-
-            int numberOfInsertions = n/3;
-            int i = 0, count = 0, elementToBeInserted = 0;
-
-            while(i<n) {
-                if(count < numberOfInsertions) {
-                    A[i] = elementToBeInserted;
-                    count++;
-                } else {
-                    elementToBeInserted = elementToBeInserted + 1;
-                    A[i] = elementToBeInserted;
-                    count = 1;
-                }
-                i++;
-            }
-            return A;
+        int[] r = optimized(A);
+        for(int i=0; i<r.length; i++) {
+            System.out.print(r[i] + " ");
         }
     }
 
+    // Time complexity - O(n) -
+    public static int[] optimized(int[] A) {
+
+        int n = A.length;
+        int zeroInsertPosition=0, twoInsertPosition=n-1;
+
+        for(int i=0; i<=twoInsertPosition; ) {
+
+            if(A[i] == 0) {
+                int temp = A[zeroInsertPosition];
+                A[zeroInsertPosition] = A[i];
+                A[i] = temp;
+                zeroInsertPosition++;
+                i++;
+            } else if(A[i] == 2) {
+                int temp = A[twoInsertPosition];
+                A[twoInsertPosition] = A[i];
+                A[i] = temp;
+                twoInsertPosition--;
+            } else {
+                i++;
+            }
+        }
+
+        return A;
+    }
+    // Time complexity - O(n)
+    public static int[] sortColors(int[] A) {
+
+        int redCount=0, whiteCount=0, blueCount=0;
+        for(int i=0; i<A.length; i++) {
+
+            if(A[i] == 0) {
+                redCount++;
+            } else if(A[i] == 1) {
+                whiteCount++;
+            } else if(A[i] == 2) {
+                blueCount++;
+            }
+        }
+
+        int redInitial=0, whiteInitial=0, blueInitial=0;
+        for(int i=0; i<A.length; i++) {
+
+            if(redInitial<redCount) {
+                A[i] = 0;
+                redInitial++;
+            } else if(whiteInitial<whiteCount) {
+                A[i] = 1;
+                whiteInitial++;
+            } else if(blueInitial<blueCount) {
+                A[i] = 2;
+                blueInitial++;
+            }
+        }
+
+        return A;
+    }
 }
