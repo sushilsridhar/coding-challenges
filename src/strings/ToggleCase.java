a2e3b4e99dc68a6c7609f7a1c48324a33afe6957
Sushil <sushilsridhar16@gmail.com>
2022-01-13 15:52:06 +0530

    ToggleCase optimized

--- src/strings/ToggleCase.java
+++ src/strings/ToggleCase.java
@@ -18,8 +18,13 @@ package strings;
     '9' - 57
 
     observation:
-    A - 65, a - 97, 97-65 = 32
+    A - 65, a - 97, a-A -> 97-65 = 32
     char element = 'A'+32 -> a
+
+    optimized:
+    'Z' -> 90  -> 1 0 1 1 0 1 0
+    'z' -> 122 -> 1 1 1 1 0 1 0, only one bit change
+        XOR 32 -> 0 1 0 0 0 0 0, to toggle cases
  */
 public class ToggleCase {
     public static void main(String []args) {
@@ -27,7 +32,37 @@ public class ToggleCase {
         String str = "aBcDeA"; // ans - AbCdEa
 
         System.out.println(toggleCase(str));
+        System.out.println(solution(str));
+        System.out.println(optimized(str));
+    }
 
+    private static String optimized(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for(int i=0; i<str.length(); i++) {
+            char ans = (char) (str.charAt(i) ^ 32);
+            builder.append(ans);
+        }
+
+        return builder.toString();
+    }
+
+    private static String solution(String str) {
+        StringBuilder builder = new StringBuilder();
+
+        for(int i=0; i<str.length(); i++) {
+            char currChar = str.charAt(i);
+
+            if(currChar >= 'A' && currChar <='Z') {
+                char ans = (char) (currChar+('a'-'A'));
+                builder.append(ans);
+            } else if(currChar >= 'a' && currChar <='z') {
+                char ans = (char) (currChar-('a'-'A'));
+                builder.append(ans);
+            }
+        }
+
+        return builder.toString();
     }
 
     private static String toggleCase(String str) {
