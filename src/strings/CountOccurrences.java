8a538940861b52faaa1b65ebbae073b106ef7bc7
Sushil <sushilsridhar16@gmail.com>
2022-01-18 12:29:24 +0530

    string problems

--- src/strings/CountOccurrences.java
+++ src/strings/CountOccurrences.java
@@ -1,2 +1,27 @@
-package strings;public class CountOccurrences {
+package strings;
+
+/*
+    Find number of occurrences of bob in the string A consisting of lowercase english alphabets.
+
+    "abobc" - 1
+    "bobob" - 2
+ */
+public class CountOccurrences {
+
+    public static void main(String []args) {
+
+        String str = "bobob";
+
+        System.out.println(solve(str));
+    }
+
+    public static int solve(String A) {
+        int count = 0;
+        for(int i=1; i<A.length()-1; i++) {
+            if(A.charAt(i) == 'o' && A.charAt(i-1) == 'b' && A.charAt(i+1) == 'b') {
+                count++;
+            }
+        }
+        return count;
+    }
 }
--- src/strings/StringOperations.java
+++ src/strings/StringOperations.java
@@ -1,2 +1,43 @@
-package strings;public class StringOperations {
+package strings;
+
+/*
+    Akash likes playing with strings. One day he thought of applying following operations on the string in the given order:
+
+    Concatenate the string with itself.
+    Delete all the uppercase letters.
+    Replace each vowel with '#'.
+    You are given a string A of size N consisting of lowercase and uppercase alphabets. Return the resultant string after applying the above operations.
+
+    NOTE: 'a' , 'e' , 'i' , 'o' , 'u' are defined as vowels.
+
+    String str = "AbcaZeoB";
+    "bc###bc###"
+
+ */
+public class StringOperations {
+
+    public static void main(String []args) {
+
+        String str = "AbcaZeoB";
+
+        System.out.println(solve(str));
+    }
+
+    public static String solve(String A) {
+        StringBuilder builder = new StringBuilder();
+
+        for(int i=0; i<A.length(); i++) {
+            char currChar = A.charAt(i);
+
+            if(currChar >= 'a' && currChar <= 'z') {
+                if(currChar == 'a' || currChar == 'e' || currChar == 'i' || currChar == 'o' || currChar == 'u') {
+                    builder.append('#');
+                } else {
+                    builder.append(currChar);
+                }
+            }
+        }
+        builder.append(builder);
+        return builder.toString();
+    }
 }
