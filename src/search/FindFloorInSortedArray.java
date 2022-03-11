44585414e335a15691b145bad8d499b5cd833a09
Sushil <sushilsridhar16@gmail.com>
2022-03-11 10:53:59 +0530

    find floor binary search

--- src/search/FindFloorInSortedArray.java
+++ src/search/FindFloorInSortedArray.java
@@ -1,2 +1,48 @@
-package search;public class FindFloorInSortedArray {
+package search;
+
+/*
+    Given a sorted array, find the floor of a given number k
+    floor -> greatest number <= k
+
+    tc: O(logn), sc: O(1)
+ */
+public class FindFloorInSortedArray {
+
+    public static void main(String[] args) {
+
+        int a[] = { -5, 2, 3, 6, 9, 10, 11, 14, 18};
+        System.out.println(search(a, 1));
+        System.out.println(search(a, 3));
+        System.out.println(search(a, -6));
+        System.out.println(search(a, 12));
+        System.out.println(search(a, 19));
+        System.out.println(search(a, 8));
+        System.out.println(search(a, 15));
+
+    }
+
+    private static int search(int[] a , int k) {
+
+        if(k < a[0]) { // no floor exist for this k
+            return Integer.MIN_VALUE;
+        }
+
+        int l = 0;
+        int r = a.length-1;
+        int ans = Integer.MIN_VALUE;
+
+        while(l<=r) {
+            int mid = (l + r)/2;
+
+            if(a[mid] == k) {
+                return a[mid];
+            } else if(a[mid] > k) {
+                r = mid - 1;
+            } else if(a[mid] < k) {
+                ans = a[mid];
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
 }
--- src/search/SearchInSortedArray.java
+++ src/search/SearchInSortedArray.java
@@ -1,2 +1,36 @@
-package search;public class SearchInSortedArray {
+package search;
+
+/*
+    Given a sorted array with distinct elements, search and return the index of the element k. if not present return -1
+
+    tc: O(logn)
+    sc: O(1)
+ */
+public class SearchInSortedArray {
+
+    public static void main(String[] args) {
+
+        int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(binarySearch(a, 3));
+    }
+
+    private static int binarySearch(int[] a, int k) {
+
+        int l = 0;
+        int r = a.length-1;
+
+        while(l<=r) {
+            int mid = (l+r)/2;
+
+            if(a[mid] == k) {
+                return mid;
+            } else if(a[mid] > k) {
+                r = mid - 1;
+            } else if(a[mid] < k) {
+                l = mid + 1;
+            }
+        }
+
+        return -1;
+    }
 }
