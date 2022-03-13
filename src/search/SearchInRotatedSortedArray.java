fd716adfb90ae36cdd2f96fc22aceeb53b2dbe1e
Sushil <sushilsridhar16@gmail.com>
2022-03-13 19:34:34 +0530

    binary search

--- src/search/SearchInRotatedSortedArray.java
+++ src/search/SearchInRotatedSortedArray.java
@@ -15,6 +15,52 @@ public class SearchInRotatedSortedArray {
             //      0  1  2  3  4  5  6  7
         int a[] = { 4, 5, 6, 7, 0, 1, 2, 3};
         System.out.println(search(a, 3));
+
+        System.out.println(efficient(a, 3));
+
+    }
+
+    // tc: O(logn)
+    private static int efficient(final int[] A, int B) {
+        int ans = -1;
+        int k = findRotatedIndexVal(A);
+        int n = A.length-1;
+
+        if(k != 0) {
+            if(B >= A[0]) {
+                ans = binarySearch(A, 0, k, B);
+            } else {
+                ans = binarySearch(A, k+1, n, B);
+            }
+        } else {
+            ans = binarySearch(A, 0, n, B);
+        }
+
+        return ans;
+    }
+
+    private static int findRotatedIndexVal(int[] a) {
+        int k = 0;
+        int l = 0;
+        int n = a.length-1;
+        int r = n;
+
+        while(l<=r) {
+            int mid = (l+r)/2;
+
+            if(a[mid] > a[0]) {
+                k = mid;
+                l = mid + 1;
+            } else if(a[mid] < a[0]) {
+                r = mid - 1;
+            }
+        }
+
+        if(k == n) {
+            return 0;
+        }
+
+        return k;
     }
 
     // tc: O(nlogn)
