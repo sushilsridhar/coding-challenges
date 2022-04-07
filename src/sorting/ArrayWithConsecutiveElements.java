556602f98b8e802660b576e13968bef5eccd6319
Sushil <sushilsridhar16@gmail.com>
2022-04-07 20:01:21 +0530

    trees and sorting

---
+++ src/sorting/ArrayWithConsecutiveElements.java
@@ -0,0 +1,66 @@
+package sorting;
+
+import java.util.Arrays;
+
+/*
+    Given an array of positive integers A, check and return whether the array elements are consecutive or not.
+    NOTE: Try this with O(1) extra space.
+
+    tc: O(nlogn)
+ */
+public class ArrayWithConsecutiveElements {
+
+    public static void main(String[] args) {
+
+        int a[] = {3, 2, 1, 4, 5};
+
+        System.out.println(bruteForce(a));
+        System.out.println(sortingSolution(a));
+    }
+
+    // tc: O(nlogn)
+    private static int sortingSolution(int[] A) {
+
+        Arrays.sort(A);
+
+        for(int i=0; i<A.length-1; i++) {
+            if(!(A[i+1] - A[i] == 1)) {
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    // tc: O(n^2)
+    private static int bruteForce(int[] A) {
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<A.length; i++) {
+            if(A[i] < min) {
+                min = A[i];
+            }
+        }
+
+        int count = 0;
+        while(count < A.length) {
+            boolean minFound = false;
+            for(int i=0; i<A.length; i++) {
+
+                if(min == A[i]) {
+                    minFound = true;
+                    break;
+                }
+            }
+
+            if(!minFound) {
+                return 0;
+            }
+
+            count++;
+            min++;
+        }
+
+        return 1;
+    }
+}
---
+++ src/trees/CountingTheNodes.java
@@ -0,0 +1,72 @@
+package trees;
+
+/*
+    Given the root of a tree A with each node having a certain value, find the count of nodes with more value than all its ancestor.
+
+
+            4
+       5         2
+              3     6
+
+    4,5,6 -> count is 3
+
+    approach:
+    compare root with left and right child
+    if 2 is less than 4, then compare 4 and 6 instead of 2 and 6
+
+    tc: O(n), n is the total number of nodes
+ */
+public class CountingTheNodes {
+
+    static class TreeNode {
+        int data;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = setup();
+
+        System.out.println(countGreaterNodes(root, 0));
+    }
+
+    private static int countGreaterNodes(TreeNode A, int greaterRoot) {
+
+        if(A == null) {
+            return 0;
+        }
+
+        int count = 0;
+        if(A.data > greaterRoot) {
+            count++;
+            count = count + countGreaterNodes(A.left, A.data) + countGreaterNodes(A.right, A.data);
+        } else {
+            count = count + countGreaterNodes(A.left, greaterRoot) + countGreaterNodes(A.right, greaterRoot);
+        }
+
+        return count;
+    }
+
+    public static TreeNode setup() {
+
+        TreeNode root = new TreeNode(4);
+
+        TreeNode two = new TreeNode(2);
+        TreeNode three = new TreeNode(3);
+        TreeNode five = new TreeNode(5);
+        TreeNode six = new TreeNode(6);
+
+        root.left = five;
+        root.right = two;
+
+        two.left = three;
+        two.right = six;
+
+        return root;
+    }
+
+}
