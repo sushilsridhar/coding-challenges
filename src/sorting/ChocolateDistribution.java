ce060000634e6acf311ed9d863c60c79544e7f8b
Sushil <sushilsridhar16@gmail.com>
2022-04-08 20:31:29 +0530

    sorting problems

---
+++ src/sorting/ChocolateDistribution.java
@@ -0,0 +1,57 @@
+package sorting;
+
+import java.util.Arrays;
+
+/*
+    Given an array A of N integers where each value represents number of chocolates in a packet.
+
+    i-th can have A[i] number of chocolates.
+
+    There are B number students, the task is to distribute chocolate packets following below conditions:
+
+    1. Each student gets one packet.
+    2. The difference between the number of chocolates in packet with maximum chocolates and packet with minimum chocolates given to the students is minimum.
+    Return the minimum difference (that can be achieved) between maximum and minimum number of chocolates distributed.
+
+    A : [3, 4, 1, 9, 56, 7, 9, 12]
+    B : 5
+
+    Minimum Difference is 6
+    The set goes like 3,4,7,9,9 and the output is 9-3 = 6
+
+    approach:
+    distribute any 5 packets to 5 students, such that condition is met
+    array needs to be sorted, so that we can distribute chocolate to B students such that difference is minimum,
+    1 3 4 7 9 9 --> for 4 diff between 3 and 4, 4 and 7 is small, this happens only after sorting
+
+    tc: O(nlogn)
+ */
+public class ChocolateDistribution {
+
+    public static void main(String[] args) {
+
+        int a[] = {3, 4, 1, 9, 56, 7, 9, 12};
+        System.out.println(solve(a, 5)); // ans -> 6
+    }
+
+    private static int solve(int[] A, int B) {
+
+        if(B == 0 || A.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(A);
+
+        int n = A.length;
+        int ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<=n-B; i++) {
+            int j = B-1 + i;
+            ans = Math.min(ans, A[j] - A[i]);
+        }
+
+        return ans;
+    }
+}
+
+
---
+++ src/sorting/MaxMod.java
@@ -0,0 +1,49 @@
+package sorting;
+
+/*
+    Given an array A of size N, Groot wants you to pick 2 indices i and j such that
+
+    1 <= i, j <= N
+    A[i] % A[j] is maximum among all possible pairs of (i, j).
+    Help Groot in finding the maximum value of A[i] % A[j] for some i, j.
+
+    approach:
+
+    anynumber % 10 => result will be in range of 0-9 (0- (mod-1))
+    so mod has to be max value in the array, and secondmax is the numerator to have the max mod value
+
+    tc: O(n)
+ */
+public class MaxMod {
+    public static void main(String[] args) {
+
+        int a[] = {1, 2, 44, 3};
+        System.out.println(solve(a));
+
+        int b[] = {5, 5, 5, 5};
+        System.out.println(solve(b));
+    }
+
+    // tc: O(n)
+    private static int solve(int[] A) {
+
+        int max = Integer.MIN_VALUE;
+        int secondMax = max;
+
+        for(int i=0; i<A.length; i++) {
+
+            if(A[i] > max) {
+                secondMax = max;
+                max = A[i];
+            } else if((A[i] != max) && (A[i] > secondMax)) {
+                secondMax = A[i];
+            }
+        }
+
+        if(secondMax == Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return secondMax%max;
+    }
+}
