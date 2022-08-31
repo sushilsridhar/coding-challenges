e8f74878d999d14d3d6131d680b0a10e7074fb96
sushilsridhar <sushil.sridhar.sh@gmail.com>
2022-08-31 16:10:52 +0530

    two pointers

--- src/twopointers/MultipleSortedArraysClosestPairs.java
+++ src/twopointers/MultipleSortedArraysClosestPairs.java
@@ -1,11 +1,89 @@
 package twopointers;
 
 /*
-    TODO
+    Given two sorted arrays of distinct integers, A and B, and an integer C, find and return the pair whose sum is closest to C and the pair has one element from each array.
+    More formally, find A[i] and B[j] such that abs((A[i] + B[j]) - C) has minimum value.
+    If there are multiple solutions find the one with minimum i and even if there are multiple values of j for the same i then return the one with minimum j.
+    Return an array with two elements {A[i], B[j]}
+
+     A = [1, 2, 3, 4, 5]
+     B = [2, 4, 6, 8]
+     C = 9
+
+     There are three pairs: (1, 8), (3, 6), (5, 4), that gives the minimum value.
+     Since we have to return the value with minimum i and then with minimum j. We will return [1, 8]
+
+     brute force,
+     find which sum is the closest to the target
+
+     tc: O(n^2)
+     sc: O(1)
+
+     two pointers,
+     since both arrays are sorted, iterate both array using two pointers,
+     one pointer at start of one array, another pointer at end of another array,
+
+     if sum > target , decrease second pointer, else increase first pointer
+
+     tc: O(n+m), size of two arrays
+     sc: O(1)
  */
 public class MultipleSortedArraysClosestPairs {
 
-    public int[] solve(int[] A, int[] B, int C) {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 5};
+        int b[] = {2, 4, 6, 8};
+        int c = 9;
+
+        for(int i: bruteForce(a,b,c)) {
+            System.out.print(i+" ");
+        }
+
+        System.out.println("");
+
+        for(int i: twoPointers(a,b,c)) {
+            System.out.print(i+" ");
+        }
+    }
+
+    private static int[] twoPointers(int[] A, int[] B, int C) {
+
+        int ans = Integer.MAX_VALUE;
+        int[] r = new int[2];
+
+        int target = C;
+        int i = 0;
+        int j = B.length-1;
+
+        int distanceBetweenAnsAndTarget = Math.abs(ans - target);
+
+        while(i<A.length && j>=0) {
+            int sum = A[i] + B[j];
+            int distanceBetweenSumAndTarget = Math.abs(sum - target);
+
+            if(distanceBetweenSumAndTarget < distanceBetweenAnsAndTarget) {
+                distanceBetweenAnsAndTarget = distanceBetweenSumAndTarget;
+                r[0] = A[i];
+                r[1] = B[j];
+            } else if(distanceBetweenSumAndTarget == distanceBetweenAnsAndTarget) {
+                if(A[i] <= r[0] && B[j] <= r[1]) {
+                    r[0] = A[i];
+                    r[1] = B[j];
+                }
+            }
+
+            if(sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+
+        }
+
+        return r;
+    }
+
+    private static int[] bruteForce(int[] A, int[] B, int C) {
 
         int ans = Integer.MAX_VALUE;
         int[] r = new int[2];
--- src/twopointers/MultipleSortedArraysMinDiffThreePointers.java
+++ src/twopointers/MultipleSortedArraysMinDiffThreePointers.java
@@ -1,9 +1,48 @@
 package twopointers;
 
 /*
-    TODO
+    Given three sorted arrays A, B and Cof not necessarily same sizes.
+    Calculate the minimum absolute difference between the maximum and minimum number from the triplet a, b, c
+    such that a, b, c belongs arrays A, B, C respectively. i.e. minimize | max(a,b,c) - min(a,b,c) |
+
+    A : [ 1, 4, 5, 8, 10 ]
+    B : [ 6, 9, 15 ]
+    C : [ 2, 3, 6, 6 ]
+
+    We get the minimum difference for a=5, b=6, c=6 as | max(a,b,c) - min(a,b,c) | = |6-5| = 1
+
+    brute force, try all combinations
+
+    tc: O(n^3)
+    sc: O(1)
+
+    three pointers,
+    each pointer at start of a array,
+
+    | max(a,b,c) - min(a,b,c) |
+
+    | max(1,6,2) - min(1,6,2) | , max is 6 and min is 1,
+
+    6-1 = 5, to reduce the absolute difference, decrease the max value which is 6, or increase the min value which is 1,
+    decreasing max is not possible, so increase the min value,
+
+    find which array contributes to the min value, increase the index of that array
+
+    tc: O(n+m+l), size of all three arrays
+    sc: O(1)
+
  */
 public class MultipleSortedArraysMinDiffThreePointers {
+
+    public static void main(String[] args) {
+        int a[] = {1, 4, 5, 8, 10};
+        int b[] = {6, 9, 15};
+        int c[] = {2, 3, 6, 6};
+
+        System.out.println(bruteForce(a,b,c));
+        System.out.println(threePointers(a,b,c));
+
+    }
 
     private static int threePointers(int[] A, int[] B, int[] C) {
         int ans = Integer.MAX_VALUE;
